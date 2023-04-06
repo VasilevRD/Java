7630eda12a1f56010ca8f0a4032dcc6d3f122a1c
VasilevRD <113347057+VasilevRD@users.noreply.github.com>
2023-04-06 13:04:33 +0300

    Add files via upload

---
+++ java_1.js
@@ -0,0 +1,28 @@
+"Привет, Java!"
+var myThing;
+myThing=5;
+myThing="это строка";
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет,";
+var myName = "Ник";
+greeting + myName;
+myName.length;
+var name_spid, name_vich, num_spid_mon, num_spid_tues, 
+num_spid_wedn, num_vich_mon, num_vich_tues, num_vich_wedn, num_spid_week, num_vich_week;
+name_vich="ВИЧ";
+name_spid="СПИД";
+var  num_spid_mon = 2;
+var num_spid_tues = 3;
+var num_spid_wedn = 4;
+num_spid_week = num_spid_mon + num_spid_tues + num_spid_wedn;
+var num_vich_mon = 0;
+var num_vich_tues = 1;
+var num_vich_wedn = 2;
+num_vich_week = num_vich_mon + num_vich_tues + num_vich_wedn;
+name_vich + num_vich_week + name_spid + num_spid_week;
+
+
+
